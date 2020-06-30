a30311e66b78eba10f56ad61db43aeb5880404e1
sowasred <muldurozan@gmail.com>
2020-06-30 11:30:59 -0400

    ga installed

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,27 @@ module.exports = {
     author: `@ozanmuldur`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-130181351-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "ozanmuldur.com",
+      },
+    },
     `gatsby-plugin-react-helmet`,
 
     {
