debbd0325b0186292908e0060ba41ff3df2b7d51
sowasred <muldurozan@gmail.com>
2020-09-10 23:19:00 -0400

    gatsby manifest changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,8 +85,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Ozan Muldur Personal Website`,
+        short_name: `Ozan Muldur`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
