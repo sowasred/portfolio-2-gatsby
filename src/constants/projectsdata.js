eb4876d419cd87ec5f4253c63cf7a8ab24fc03e5
sowasred <muldurozan@gmail.com>
2020-09-11 17:50:07 -0400

    projects style changed

--- src/constants/projectsdata.js
+++ src/constants/projectsdata.js
@@ -30,7 +30,7 @@ import Pr3 from "../images/derry123.png"
 import Pr4 from "../images/temarrr12.png"
 import Pr5 from "../images/dkmob12.png"
 import Pr6 from "../images/tuber12.png"
-import Pr7 from "../images/handyman12.png"
+import Pr7 from "../images/handyman123.png"
 
 export default [
   {
@@ -57,6 +57,21 @@ export default [
     link: "https://www.balodana.com/",
   },
   {
+    name: "THE DIAMOND STORE",
+    background: Pr5,
+    collabtype: "Collabrative Work",
+    description:
+      "Jewelry manufacturer from India converted their front-end technology from Java-Spring to React for sake of speed and reliability.",
+    techImages: [
+      { img: react, alt: "React" },
+      { img: material, alt: "material ui" },
+      { img: sass, alt: "Sass" },
+      { img: figma, alt: "figma" },
+    ],
+    tasks: ["Design Implemantations"],
+    link: "https://www.thediamondkart.com/",
+  },
+  {
     name: "Avetti Demo Marketplace",
     background: Pr2,
     collabtype: "Collabrative Work",
@@ -74,25 +89,22 @@ export default [
     link: "https://demob2b3shop.avetti.io/",
   },
   {
-    name: "Derry",
-    background: Pr3,
-    collabtype: "Collabrative Work",
+    name: "GTA - Handyman",
+    background: Pr7,
+    collabtype: "Individual Work",
     description:
-      "Derry is a Leather Jacket Store for leather fashion chasers all around the world. Thanks to its modern web stack, it allows users to shop in a pretty fast and secure environment.",
+      "Company called GTA - Handyman are giving renovation services all around Toronto GTA. I've built their WordPress site by creating a new WordPress theme from scratch according to their demands.",
     techImages: [
-      { img: gatsbyimg, alt: "Gatsby.JS" },
-      { img: nodejs, alt: "Node.js" },
-      { img: graphqlimg, alt: "graphql" },
-      { img: mongodb, alt: "mongodb" },
-      { img: strapi, alt: "CMS" },
-      { img: aws, alt: "AWS" },
+      { img: php, alt: "PHP" },
+      { img: wordpress, alt: "WordPress" },
+      { img: css, alt: "CMS" },
     ],
     tasks: [
-      "Gatsby Store Developing",
-      "Headless CMS Strapi Integration",
-      "Deploying On AWS ",
+      "WordPress Theme Creation",
+      "WordPress Website Developing",
+      "Search Engine Optimization",
     ],
-    link: "https://jacketstore.netlify.app/",
+    link: "https://www.handymangta.ca/",
   },
   {
     name: "teamAR",
@@ -109,21 +121,6 @@ export default [
     ],
     tasks: ["Progressive Web App", "CMS Integration", "Design Implementations"],
     link: "https://teamar.ca",
-  },
-  {
-    name: "THE DIAMOND STORE",
-    background: Pr5,
-    collabtype: "Collabrative Work",
-    description:
-      "Jewelry manufacturer from India converted their front-end technology from Java-Spring to React for sake of speed and reliability.",
-    techImages: [
-      { img: react, alt: "React" },
-      { img: material, alt: "material ui" },
-      { img: sass, alt: "Sass" },
-      { img: figma, alt: "figma" },
-    ],
-    tasks: ["Design Implemantations"],
-    link: "https://www.thediamondkart.com/",
   },
   {
     name: "Tuber Support",
@@ -146,21 +143,24 @@ export default [
     link: "https://www.tubersupport.com/",
   },
   {
-    name: "GTA - Handyman",
-    background: Pr7,
-    collabtype: "Individual Work",
+    name: "Derry",
+    background: Pr3,
+    collabtype: "Collabrative Work",
     description:
-      "Company called GTA - Handyman are giving renovation services all around Toronto GTA. I've built their WordPress site by creating a new WordPress theme from scratch according to their demands.",
+      "Derry is a Leather Jacket Store for leather fashion chasers all around the world. Thanks to its modern web stack, it allows users to shop in a pretty fast and secure environment.",
     techImages: [
-      { img: php, alt: "PHP" },
-      { img: wordpress, alt: "WordPress" },
-      { img: css, alt: "CMS" },
+      { img: gatsbyimg, alt: "Gatsby.JS" },
+      { img: nodejs, alt: "Node.js" },
+      { img: graphqlimg, alt: "graphql" },
+      { img: mongodb, alt: "mongodb" },
+      { img: strapi, alt: "CMS" },
+      { img: aws, alt: "AWS" },
     ],
     tasks: [
-      "WordPress Theme Creation",
-      "WordPress Website Developing",
-      "Search Engine Optimization",
+      "Gatsby Store Developing",
+      "Headless CMS Strapi Integration",
+      "Deploying On AWS ",
     ],
-    link: "https://www.handymangta.ca/",
+    link: "https://jacketstore.netlify.app/",
   },
 ]
