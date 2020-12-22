bd5aa75f3d1173238440152d87662889f14355f0
sowasred <muldurozan@gmail.com>
2020-12-21 23:11:48 -0500

    blog post new order

--- src/components/blogposts.js
+++ src/components/blogposts.js
@@ -15,7 +15,32 @@ const Blogposts = () => (
       <h2>Recent Blog Posts</h2>
       <div className="underline"></div>
     </div>
+    <Blogpostcard
+      title="Why Strapi CMS in 2021"
+      description="There is a great chance that you might have heard headless CMS. In this article, I'm reasoning my favorite one."
+      img={Blogpost4Pic}
+      date="2020-12-21"
+      minutes="6"
+      slug="/blog/whystrapi"
+    />
 
+    <Blogpostcard
+      title="Which Database Best Fit For You?"
+      description="When you start a new project, database choice is crtical, before making decision you'd better check this out."
+      img={Blogpost3Pic}
+      date="2020-12-18"
+      minutes="6"
+      slug="/blog/sqlvsnosql"
+    />
+    <Blogpostcard
+      title="Augmented Reality Usage in Businesses"
+      description="AR can be extremely helpful for the customers in terms of making
+              right decision."
+      img={Blogpost2Pic}
+      date="2020-06-16"
+      minutes="4"
+      slug="/blog/arinbusiness"
+    />
     <div className="section-center blog-center">
       <Blogpostcard
         title="Get Into JAMStack"
@@ -25,32 +50,6 @@ const Blogposts = () => (
         date="2020-04-14"
         minutes="3"
         slug="/blog/jamstackvsmern"
-      />
-
-      <Blogpostcard
-        title="Augmented Reality Usage in Businesses"
-        description="AR can be extremely helpful for the customers in terms of making
-              right decision."
-        img={Blogpost2Pic}
-        date="2020-06-16"
-        minutes="4"
-        slug="/blog/arinbusiness"
-      />
-      <Blogpostcard
-        title="Which Database Best Fit For You?"
-        description="When you start a new project, database choice is crtical, before making decision you'd better check this out."
-        img={Blogpost3Pic}
-        date="2020-12-18"
-        minutes="6"
-        slug="/blog/sqlvsnosql"
-      />
-      <Blogpostcard
-        title="Why Strapi CMS in 2021"
-        description="There is a great chance that you might have heard headless CMS. In this article, I'm reasoning my favorite one."
-        img={Blogpost4Pic}
-        date="2020-12-21"
-        minutes="6"
-        slug="/blog/whystrapi"
       />
     </div>
   </section>
