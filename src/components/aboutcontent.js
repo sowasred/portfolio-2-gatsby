982652c25049d67f3f147e6b97937e700861a8aa
Ozan <ozan@Ozans-Mac-mini.local>
2022-06-12 08:42:06 -0400

    images added to about page

--- src/components/aboutcontent.js
+++ src/components/aboutcontent.js
@@ -1,5 +1,6 @@
 import React from "react"
-
+import internImage from "../images/ozan-muldur-istanbul-technical-university-internship.jpeg"
+import engineerImage from "../images/engineer-ozan-muldur.jpeg"
 
 const AboutContent = () => (
   <section className="section single-page">
@@ -10,7 +11,15 @@ const AboutContent = () => (
     <div className="section-center page-info">
       <div className="section-center timeline-center">
         <article className="timeline-item">
-          <h4>Istanbul Technical University </h4>
+          <h4>
+            <a
+              className="aboutPageLink"
+              target="_blank"
+              href="https://www.itu.edu.tr/en"
+            >
+              Istanbul Technical University
+            </a>
+          </h4>
           <h4 className="timeline-title">Ocean Engineering | 2010 - 2015</h4>
           <p className="item-text">
             I found my passion later in life but that is okay. At the end of my
@@ -19,23 +28,50 @@ const AboutContent = () => (
             real dimensions and being able to walk on it would give me joy and
             motivation for life.
           </p>
+          <img
+            className="aboutPageImage"
+            alt="Ozan Muldur istanbul technical university internship picture"
+            src={internImage}
+          ></img>
           <span className="number">1</span>
         </article>
         <article className="timeline-item">
-          <h4>Gozuyilmaz Eng. Ltd. </h4>
+          <h4>
+            <a
+              className="aboutPageLink"
+              target="_blank"
+              href="https://www.izmirshipyard.com/"
+            >
+              Izmir Shipyard Engineered by Gozuyilmaz
+            </a>
+          </h4>
           <h4 className="timeline-title">Design Engineer | 2015 – 2017</h4>
           <p className="item-text">
             When I started working in the field, I quickly figured out that it
             is fun and all, but it is not the sole job that I want to do for the
             rest of my life. In the meantime, I was coming across programming
-            videos on YouTube and even started to code along with some of them.<br></br>
+            videos on YouTube and even started to code along with some of them.
+            <br></br>
             After a week I was hooked on being able to create whatever I want in
             terms of coding and this made me dig into deep more and more...
           </p>
+          <img
+            className="aboutPageImage"
+            alt="Ozan Muldur engineer picture"
+            src={engineerImage}
+          ></img>
           <span className="number">2</span>
         </article>
         <article className="timeline-item">
-          <h4>Georgian College</h4>
+          <h4>
+            <a
+              className="aboutPageLink"
+              target="_blank"
+              href="https://www.georgiancollege.ca/"
+            >
+              Georgian College
+            </a>
+          </h4>
           <h4 className="timeline-title">Computer Programmer | 2017 – 2019</h4>
           <p className="item-text">
             Those desires have led me to study abroad in Canada. It was a great
@@ -43,9 +79,16 @@ const AboutContent = () => (
           </p>
           <span className="number">3</span>
         </article>
-
         <article className="timeline-item">
-          <h4>Avetti Commerce</h4>
+          <h4>
+            <a
+              className="aboutPageLink"
+              target="_blank"
+              href="https://www.avetticommerce.com/"
+            >
+              Avetti Commerce
+            </a>
+          </h4>
           <h4 className="timeline-title">Front-end Developer | 2019 – 2021</h4>
           <p className="item-text">
             Thanks to my React knowledge in web application development helped
@@ -57,15 +100,25 @@ const AboutContent = () => (
           </p>
           <span className="number">4</span>
         </article>
-        
         <article className="timeline-item">
-          <h4>ResortSuite</h4>
-          <h4 className="timeline-title">Full Stack Developer | 2021 - Current</h4>
+          <h4>
+            <a
+              className="aboutPageLink"
+              target="_blank"
+              href="https://www.resortsuite.com/"
+            >
+              ResortSuite
+            </a>
+          </h4>
+          <h4 className="timeline-title">
+            Full Stack Developer | 2021 - Current
+          </h4>
           <p className="item-text">
-            In my current position I'm working on company's various hospitality managements 
-            softwares as a full-stack dev. Dealing with multiple projects based on different 
-            technology stacks has made me more versatile since I've started. I'm also grateful 
-            for being part of such a great team and culture in ResortSuite. 
+            In my current position I'm working on company's various hospitality
+            managements softwares as a full-stack dev. Dealing with multiple
+            projects based on different technology stacks has made me more
+            versatile since I've started. I'm also grateful for being part of
+            such a great team and culture in ResortSuite.
           </p>
           <a href="mailto:ozanmuldur@outlook.com" className="btn hero-btn">
             Contact Me
